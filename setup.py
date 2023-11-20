eda93ff5fa57b53fc777876fd90dd17a63417477
gordbegli <ggordbegli@gmail.com>
2023-11-20 15:54:15 -0500

    Remove Temp Script File

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='verbomate',  # Replace with your chosen package name
-    version='0.1.5',  # Initial version number
+    version='0.1.7',  # Initial version number
     packages=find_packages(),
     entry_points={
         'console_scripts': [
